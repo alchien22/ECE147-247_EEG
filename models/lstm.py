934e569771ed8db68bad549dd9645bd7193815b3
joe-lin-tech <joe.tech.lin@gmail.com>
2024-03-14 00:11:40 -0700

    update: add model save to training scripts and add conformer

--- models/lstm.py
+++ models/lstm.py
@@ -18,33 +18,47 @@ class LSTM(nn.Module):
         self.hidden_dim = hidden_dim
 
         prev_dim = input_dim
-        for dim in conv_dims:
-            self.conv.append(nn.Conv1d(prev_dim, dim, kernel_size=7, padding=3))
-            self.conv.append(nn.ELU())
-            self.conv.append(nn.MaxPool1d(kernel_size=2, stride=2))
+        for i, dim in enumerate(conv_dims):
+            self.conv.append(nn.Conv1d(prev_dim, dim, kernel_size=5, padding=2))
+            # self.conv.append(nn.ELU())
+            self.conv.append(nn.ReLU())
+            if i == 1:
+                self.conv.append(nn.MaxPool1d(kernel_size=2, stride=2))
             self.conv.append(nn.BatchNorm1d(dim))
-            self.conv.append(nn.Dropout(0.5))
+            self.conv.append(nn.Dropout(0.4))
             prev_dim = dim
         
-        self.lstm = nn.LSTM(prev_dim, hidden_dim, num_layers, batch_first=True)
-        self.out = nn.Linear(hidden_dim, 4)
+        self.lstm = nn.LSTM(prev_dim, hidden_dim, num_layers, batch_first=True, bidirectional=True)
+        self.out = nn.Linear(hidden_dim * 200 * 2, 4)
         
     def forward(self, x):
         for layer in self.conv:
             x = layer(x)
         x = torch.transpose(x, 1, 2)
+        # out, (h_n, c_n) = self.lstm(x)
+        # out = self.out(F.relu(out[:, -1]))
         out, (h_n, c_n) = self.lstm(x)
-        out = self.out(F.relu(out[:, -1]))
+        out = self.out(torch.flatten(out, 1))
         return out
 
-model = LSTM(input_dim=22, conv_dims=[32, 64], hidden_dim=128, num_layers=1)
-model.to('cpu')
+model = LSTM(input_dim=22, conv_dims=[16, 32, 48, 64], hidden_dim=64, num_layers=1)
+# model = LSTM(input_dim=22, conv_dims=[32, 64], hidden_dim=128, num_layers=1)
+model.to('mps')
 
 optimizer = Adam(model.parameters(), lr=1e-3)
 criterion = nn.CrossEntropyLoss()
 
 num_epochs = 100
 for epoch in range(num_epochs):
-    train_loss, train_acc = train(model, train_dataloader, optimizer, criterion, torch.device('cpu'))
-    val_loss, val_acc = evaluate(model, val_dataloader, criterion, torch.device('cpu'))
+    train_loss, train_acc = train(model, train_dataloader, optimizer, criterion, torch.device('mps'))
+    val_loss, val_acc = evaluate(model, val_dataloader, criterion, torch.device('mps'))
     print(f"Epoch {epoch + 1}/{num_epochs} loss: {train_loss}, acc: {train_acc}, val_loss: {val_loss}, val_acc: {val_acc}")
+    if (epoch + 1) % 5 == 0:
+        torch.save({
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'train_loss': train_loss,
+            'train_acc': train_acc,
+            'val_loss': val_loss,
+            'val_acc': val_acc
+        }, f'lstm_epoch{epoch + 1}.pt')
