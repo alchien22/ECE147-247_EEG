7e05f09e2365280e52a8c51126738ebad43c9103
Gabriel Castro <gaben24k@gmail.com>
2024-03-13 22:46:47 -0700

    add cnn attention

--- cnn_attention.py
+++ cnn_attention.py
@@ -3,60 +3,53 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 
-
-class SelfAttention(nn.Module):
-    def __init__(self, input_dim):
-        super(SelfAttention, self).__init__()
-        self.input_dim = input_dim
-        self.query = nn.Linear(input_dim, input_dim)
-        self.key = nn.Linear(input_dim, input_dim)
-        self.value = nn.Linear(input_dim, input_dim)
-        self.softmax = nn.Softmax(dim=2)
-        
-    def forward(self, x):
-        # get the query, key and value vectors
-        x = x.transpose(2, 1)
-        queries = self.query(x)
-        keys = self.key(x)
-        values = self.value(x)
-        
-        # calculate the attention scores by the matrix mult. of queries and keys
-        scores = torch.bmm(queries, keys.transpose(1, 2)) / (self.input_dim ** 0.5)
-
-        # convert scores into probabilities w/ softmax
-        attention = self.softmax(scores)
-        
-        # get the weighted values by the matrix mult. of attention scores and values
-        weighted = torch.bmm(attention, values)
-        
-        return weighted
-
 class CNN_Attention_Model(nn.Module):
     def __init__(self):
         super(CNN_Attention_Model, self).__init__()
-        self.conv1 = nn.Conv1d(22, 64, kernel_size=3, padding=1)
-        self.self_attention1 = SelfAttention(input_dim=25)
+        self.conv1 = nn.Conv1d(22, 64, kernel_size=3)
+        self.conv2 = nn.Conv1d(64, 128, kernel_size=3)
+        self.conv3 = nn.Conv1d(128, 256, kernel_size=3)
+        
+        self.pooling1 = nn.MaxPool1d(kernel_size=3)
+        self.pooling2 = nn.MaxPool1d(kernel_size=3)
+        self.pooling3 = nn.MaxPool1d(kernel_size=3)
+        
+        self.activation = nn.ReLU()
+         
         # was too lazy to implement multi-head from scratch :(
-        self.multi_head_attention1 = nn.MultiheadAttention(embed_dim=64, num_heads=4, batch_first=True)
-        self.batch_norm1 = nn.BatchNorm1d(num_features=400)
-        self.activation1 = nn.ReLU()
-        self.pooling1 = nn.MaxPool1d(kernel_size=3, padding=1)
+        self.multi_head_attention1 = nn.MultiheadAttention(embed_dim=256, num_heads=16, batch_first=True, dropout=0.5)
+
+        self.dropout = nn.Dropout1d(0.5)
         
         #200*4
-        self.fc = nn.Linear(400*22, out_features=4) 
+        self.fc1 = nn.Linear(13*256, out_features=18) 
+        self.fc2 = nn.Linear(18, out_features=4)
         
     def forward(self, x):
         # Architecture from paper
         x = self.conv1(x)
-        #print(x.shape)
+        x = self.pooling1(x)
+        #x = self.activation(x)
+        
+        x = self.conv2(x)
+        x = self.pooling2(x)
+        #x = self.activation(x)
+        
+        x = self.conv3(x)
+        x = self.pooling3(x)
+        #x = self.activation(x)
         x = x.transpose(2, 1) #(batch, feature, seq) -> (batch, seq, feature)
         x, _ = self.multi_head_attention1(x, x, x)
-        x = self.batch_norm1(x)
-        x = self.activation1(x)
-        x = self.pooling1(x)
-        #print("After pooling:", x.shape)
-        x = x.view(-1, 400*22)  # Maintain bsz dimension
-        #print("hello1")
-        x = F.softmax(self.fc(x), dim=1)
-        #print("hello2")
+        x = self.dropout(x)
+        
+        #print(x.shape)
+        x = x.reshape(-1, 13*256)  # Maintain bsz dimension
+        x = self.fc1(x)
+        x = F.softmax(self.fc2(x), dim=1)
         return x
+
+    def compute_l1_loss(self):
+        l1_loss = 0
+        for param in self.parameters():
+            l1_loss += torch.abs(param).sum()
+        return l1_loss
